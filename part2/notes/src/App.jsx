0ace9fbfcf18b5ac334ecb96d0191a5929c50078
jonahansmulcrone <jonah.mulcrone@plu.edu>
2024-11-19 22:23:23 -0800

    began axios and promises

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,7 +1,15 @@
 import { useState } from 'react'
 import Note from "./Note"
+import axios from 'axios'
 
 const App = (props) => {
+
+  const promise = axios.get('http://localhost:3001/notes')
+  console.log(promise)
+
+  const promise2 = axios.get('http://localhost:3001/foobar')
+  console.log(promise2)
+
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('a new note..')
   const [showAll, setShowAll] = useState(true)
