92ef4f2f8c9d29518b11cdc6475229a7181e6a80
jonahansmulcrone <jonah.mulcrone@plu.edu>
2024-11-28 19:56:06 -0800

    beginning part3

---
+++ part2/countries/src/Temp.jsx
@@ -0,0 +1,44 @@
+const Temp = () => {
+    return (
+        <div className="container">
+            <div className="row justify-content-md-center ">
+                <div className="col-sm" style={{border: '1px solid red'}}>
+                    Column
+                </div>
+                <div className="col" style={{border: '1px solid red'}}>
+                    Column
+                </div>
+                <div className="col" style={{border: '1px solid red'}}>
+                    Column
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm" style={{border: '1px solid red'}}>
+                    Column
+                </div>
+                <div className="col" style={{border: '1px solid red'}}>
+                    Column
+                </div>
+                <div className="col" style={{border: '1px solid red'}}>
+                    Column
+                </div>
+            </div>
+            <div className="row row-cols-4 ">
+                <div className="col-sm-6" style={{border: '1px solid red'}}>
+                    Column
+                </div>
+                <div className="col-sm-6" style={{border: '1px solid red'}}>
+                    Column
+                </div>
+                <div className="col-sm-6" style={{border: '1px solid red'}}>
+                    Column
+                </div>
+                <div className="col-sm-6" style={{border: '1px solid red'}}>
+                    Column
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Temp
--- part2/countries/src/main.jsx
+++ part2/countries/src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Temp from './Temp.jsx'
 
 createRoot(document.getElementById('root')).render(<App />)
